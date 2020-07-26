eb6c6c83040be92ec9345a304a69127fa8e8e44e
conao3 <conao3@gmail.com>
2020-07-27 02:16:38 +0900

    generate project

---
+++ smie-base.el
@@ -0,0 +1,42 @@
+;;; smie-base.el --- Major-mode collection based on SMIE  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: convenience
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/conao3/smie-base.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Major-mode collection based on SMIE.
+
+
+;;; Code:
+
+(defgroup smie-base nil
+  "Major-mode collection based on SMIE."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/smie-base.el"))
+
+(provide 'smie-base)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; smie-base.el ends here
