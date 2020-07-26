b332e2daaf5dcf7b189f23380cdf52068b745d0f
conao3 <conao3@gmail.com>
2020-07-27 02:27:57 +0900

    add new major-mode for RNC

---
+++ lisp/smie-base-rnc.el
@@ -0,0 +1,39 @@
+;;; smie-base-rnc.el --- Major-mode for RNC of SMIE collection  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; URL: https://github.com/conao3/smie-base.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Major-mode for RNC of SMIE collection.
+
+
+;;; Code:
+
+(defgroup smie-base-rnc nil
+  "Major-mode for RNC of SMIE collection."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/smie-base.el"))
+
+(provide 'smie-base-rnc)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; smie-base-rnc.el ends here
